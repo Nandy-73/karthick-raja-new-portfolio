410774c86bb626ff4acc6de2e8d646b75fff253c
Nandy-73 <119655335+Nandy-73@users.noreply.github.com>
2026-01-13 15:41:41 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/karthick-raja-portfolio/",   // ✅ MUST MATCH REPO NAME
   server: {
     host: "::",
     port: 8080,

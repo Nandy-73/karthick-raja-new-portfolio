04191932238d17d93d026b58703f67ca1c18b034
Nandy-73 <119655335+Nandy-73@users.noreply.github.com>
2026-01-13 18:29:24 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/karthick-raja-newportfolio">
+    <BrowserRouter basename="/karthick-raja-new-portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
